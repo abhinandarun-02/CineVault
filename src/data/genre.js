9cac7c99703e2cd3a32b406e5f778ef0d0e45c05
Akash Harikumar <akashharikumar2013@gmail.com>
2023-01-15 22:25:52 +0530

    converts index.html into index.js page

---
+++ src/data/genre.js
@@ -0,0 +1,37 @@
+export const genreArray = [
+    {
+        "id":"horror",
+        "location": "",
+        "image": "https://img.freepik.com/premium-photo/3d-illustration-halloween-concept-background-castle-graveyard-horror-background_685067-358.jpg?w=2000",
+        "alt": "Horror Movie Generic Image",
+        "message":"Horror"
+    },
+    {
+        "id":"romcom",
+        "location": "",
+        "image": "https://media.istockphoto.com/id/875970634/vector/romantic-film-glyphs-vector-icon.jpg?s=612x612&w=0&k=20&c=s5BrM29Vxgtc9FI3yxT56XCSsSzYq1U18uxTjnkZnc0=",
+        "alt": "Romantic Comedy Generic Image",
+        "message":"Romantic-Comedy"
+    },
+    {
+        "id":"mystery",
+        "location": "",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgN3BwZUKUNBKKDbIeSwvZr5aFHNetxLhLMHdpG5qqlnZ24xtGk2C9YLpWXXZHaZT-D9M&usqp=CAU",
+        "alt": "Mystery Movie Generic Image",
+        "message":"Mystery"
+    },
+    {
+        "id":"anime",
+        "location": "",
+        "image": "https://cloudfront-us-east-1.images.arcpublishing.com/culturacolectiva/5G6WCFYU7NACZBZ3AHZ7ICQQFU.jpg",
+        "alt": "Anime Movie Generic Image",
+        "message":"Anime"
+    },
+    {
+        "id":"action",
+        "location": "",
+        "image": "https://cdn.thecoolist.com/wp-content/uploads/2016/08/Blackhawk-Down-best-action-movie-960x540.jpg",
+        "alt": "Action Movie Generic Image",
+        "message":"Action"
+    }
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { genreArray } from '@/data/genre'
 
 export default function Home() {
   return (
-   <div></div>
+   <div>
+      <center>
+          <h1>CineVault</h1>
+          <p>INTRODUCTION------------------</p>
+      </center>
+      
+      {
+        genreArray.map((genre) =>(
+          <div>
+            <a href="/testpg" target="_blank">
+                <img width="250px" height="250px" className="card-img-top" src={genre.image} alt={genre.alt} />
+            </a>
+            <p >{genre.message}</p>
+          </div>
+        ))
+      }
+
+      <footer>
+          <a target="_blank" href="About.html">About you</a>
+          <a target="_blank" href="https://www.facebook.com/petrichor.iitpkd">Link to facebook page of petrichor</a>
+          <a target="_blank" href="https://instagram.com/petrichor.iitpkd?igshid=MDM4ZDc5MmU=">Link to instagram page of petrichor</a>
+      </footer>
+   </div>
   )
 }
