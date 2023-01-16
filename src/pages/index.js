8231cf0783e244d13c90bd55a375190c27a55350
Akash Harikumar <akashharikumar2013@gmail.com>
2023-01-16 22:11:00 +0530

    finishes layout for home-page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,28 +3,28 @@ import { genreArray } from '@/data/genre'
 
 export default function Home() {
   return (
-   <div>
-      <center>
-          <h1>CineVault</h1>
-          <p>INTRODUCTION------------------</p>
-      </center>
-      
-      {
-        genreArray.map((genre) =>(
-          <div>
-            <a href={genre.link} target="_blank">
-                <img width="250px" height="250px" className="card-img-top" src={genre.image} alt={genre.alt} />
-            </a>
-            <p >{genre.message}</p>
+   <div className='h-[100vh] w-[100%] grid grid-col-5 grid-row-1'>
+      <div className="sidebar bg-[#111111] col-span-1 flex flex-col">
+        <div className="logo flex h-[20%] w-[100%] bg-red-300 justify-center items-center">
+          <div className="test">
+            Logo Here
           </div>
-        ))
-      }
-
-      <footer>
-          <a target="_blank" href="About.html">About you</a>
-          <a target="_blank" href="https://www.facebook.com/petrichor.iitpkd">Link to facebook page of petrichor</a>
-          <a target="_blank" href="https://instagram.com/petrichor.iitpkd?igshid=MDM4ZDc5MmU=">Link to instagram page of petrichor</a>
-      </footer>
+        </div>
+        <div className="genre-select flex h-[80%] w-[100%] bg-blue-300 justify-center items-center">
+          Genres Buttons Here
+        </div>
+      </div>
+      <div className="main-content bg-[#252525] col-start-2 col-span-4 ">
+        <div className="top-bar flex justify-center items-center h-[10%] w-[100%] bg-green-300">
+          Top Search bar here
+        </div>
+        <div className="slide-show flex justify-center items-center h-[40%] w-[100%] bg-orange-300">
+          Slide show here
+        </div>
+        <div className="popular-movies flex justify-center items-center h-[50%] w-[100%] bg-violet-300">
+          Popular movies Content here
+        </div>
+      </div>
    </div>
   )
 }
