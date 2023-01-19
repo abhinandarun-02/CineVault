d5f8a94e6ecd90d243aa2da0654556edd0ad9d05
Uday Krishnan <udaykrishnanvg@gmail.com>
2023-01-19 17:07:04 +0530

    Genre page content

--- src/data/romcommvs.js
+++ src/data/romcommvs.js
@@ -3,40 +3,120 @@ export const romcomlst = [
         "id":"thg",
         "location": "",
         "image": "https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/Chhichhore_Poster.jpg/220px-Chhichhore_Poster.jpg",
+        "hovimg":"https://media3.giphy.com/media/hVUNZyf2Ugka96MVlj/giphy.gif",
         "alt": "thing",
-        "title":"5.The Thing",
-        "desc":"A research team in Antarctica is hunted by a shape-shifting alien that assumes the appearance of its victims."
+        "title":"Chhichhore",
+        "imdb":"8.3/10",
+        "dirctr":"Nitesh Tiwari",
+        "year":"2019",
+        "desc":"A tragic incident forces Anirudh, a middle-aged man, to take a trip down memory lane and reminisce his college days along with his friends, who were labelled as losers."
     },
     {
         "id":"tbd",
         "location": "",
         "image": "https://upload.wikimedia.org/wikipedia/commons/0/09/City_Lights_%281931_theatrical_poster_-_retouched%29.jpg",
+        "hovimg":"https://i.gifer.com/UB2Y.gif",
         "alt": "tumbbad",
-        "title":"4.Tumbbad",
-        "desc":"A research team in Antarctica is hunted by a shape-shifting alien that assumes the appearance of its victims."
+        "title":"City Lights",
+        "imdb":"8.5/10",
+        "dirctr":"Charles Chaplin",
+        "year":"1931",
+        "desc":"This classic story of love, tragedy and hope centers around a homeless tramp whom meets and instantly falls in love with a beautiful, albeit blind, flower seller."
     },
     {
         "id":"shg",
         "location": "",
         "image": "https://upload.wikimedia.org/wikipedia/commons/3/36/Modern_Times_poster.jpg",
+        "hovimg":"https://media4.giphy.com/media/XCxcmEQWxDdc8qsd2R/giphy.gif",
         "alt": "shining",
-        "title":"3.The Shining",
-        "desc":"A research team in Antarctica is hunted by a shape-shifting alien that assumes the appearance of its victims."
+        "title":"Modern Times",
+        "imdb":"8.5/10",
+        "dirctr":"Charles Chaplin",
+        "year":"1936",
+        "desc":"This comedic masterpiece finds the iconic Little Tramp (Charlie Chaplin) employed at a state-of-the-art factory where the inescapable machinery completely overwhelms him, and where various mishaps keep getting him sent to prison."
     },
     {
         "id":"psy",
         "location": "",
         "image": "https://upload.wikimedia.org/wikipedia/en/7/7c/Vitaebella.jpg",
+        "hovimg":"https://media.tenor.com/Otk5_f-DqiIAAAAC/film-film-historique.gif",
         "alt": "psycho",
-        "title":"2.Psycho",
-        "desc":"A research team in Antarctica is hunted by a shape-shifting alien that assumes the appearance of its victims."
+        "title":"Life is Beautiful",
+        "imdb":"8.6/10",
+        "dirctr":"Roberto Benigni",
+        "year":"1997",
+        "desc":"A gentle Jewish-Italian waiter, Guido Orefice (Roberto Benigni), meets Dora (Nicoletta Braschi), a pretty schoolteacher, and wins her over with his charm and humor. Eventually they marry and have a son, Giosue (Giorgio Cantarini)."
     },
     {
         "id":"aln",
         "location": "",
         "image": "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Forrest_Gump_poster.jpg/220px-Forrest_Gump_poster.jpg",
+        "hovimg":"https://media.tenor.com/7Rsw8Y_3TyIAAAAd/forrest-gump-run-forrest-run.gif",
         "alt": "Alien",
-        "title":"1.Alien",
-        "desc":"A research team in Antarctica is hunted by a shape-shifting alien that assumes the appearance of its victims."
+        "title":"Forrest Gump",
+        "imdb":"8.8/10",
+        "dirctr":"Robert Zemeckis", 
+        "year":"1994",
+        "desc":"Slow-witted Forrest Gump (Tom Hanks) has never thought of himself as disadvantaged, and thanks to his supportive mother (Sally Field), he leads anything but a restricted life."
+    },
+    {
+        "id":"thg",
+        "location": "",
+        "image": "https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/Chhichhore_Poster.jpg/220px-Chhichhore_Poster.jpg",
+        "hovimg":"https://media3.giphy.com/media/hVUNZyf2Ugka96MVlj/giphy.gif",
+        "alt": "thing",
+        "title":"Chhichhore",
+        "imdb":"8.3/10",
+        "dirctr":"Nitesh Tiwari",
+        "year":"2019",
+        "desc":"A tragic incident forces Anirudh, a middle-aged man, to take a trip down memory lane and reminisce his college days along with his friends, who were labelled as losers."
+    },
+    {
+        "id":"tbd",
+        "location": "",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/0/09/City_Lights_%281931_theatrical_poster_-_retouched%29.jpg",
+        "hovimg":"https://i.gifer.com/UB2Y.gif",
+        "alt": "tumbbad",
+        "title":"4. City Lights",
+        "imdb":"8.5/10",
+        "dirctr":"Charles Chaplin",
+        "year":"1931",
+        "desc":"This classic story of love, tragedy and hope centers around a homeless tramp whom meets and instantly falls in love with a beautiful, albeit blind, flower seller."
+    },
+    {
+        "id":"shg",
+        "location": "",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/3/36/Modern_Times_poster.jpg",
+        "hovimg":"https://media4.giphy.com/media/XCxcmEQWxDdc8qsd2R/giphy.gif",
+        "alt": "shining",
+        "title":"3. Modern Times",
+        "imdb":"8.5/10",
+        "dirctr":"Charles Chaplin",
+        "year":"1936",
+        "desc":"This comedic masterpiece finds the iconic Little Tramp (Charlie Chaplin) employed at a state-of-the-art factory where the inescapable machinery completely overwhelms him, and where various mishaps keep getting him sent to prison."
+    },
+    {
+        "id":"psy",
+        "location": "",
+        "image": "https://upload.wikimedia.org/wikipedia/en/7/7c/Vitaebella.jpg",
+        "hovimg":"https://media.tenor.com/Otk5_f-DqiIAAAAC/film-film-historique.gif",
+        "alt": "psycho",
+        "title":"2. Life is Beautiful",
+        "imdb":"8.6/10",
+        "dirctr":"Roberto Benigni",
+        "year":"1997",
+        "desc":"A gentle Jewish-Italian waiter, Guido Orefice (Roberto Benigni), meets Dora (Nicoletta Braschi), a pretty schoolteacher, and wins her over with his charm and humor. Eventually they marry and have a son, Giosue (Giorgio Cantarini)."
+    },
+    {
+        "id":"aln",
+        "location": "",
+        "image": "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Forrest_Gump_poster.jpg/220px-Forrest_Gump_poster.jpg",
+        "hovimg":"https://media.tenor.com/7Rsw8Y_3TyIAAAAd/forrest-gump-run-forrest-run.gif",
+        "alt": "Alien",
+        "title":"1. Forrest Gump",
+        "imdb":"8.8/10",
+        "dirctr":"Robert Zemeckis", 
+        "year":"1994",
+        "desc":"Slow-witted Forrest Gump (Tom Hanks) has never thought of himself as disadvantaged, and thanks to his supportive mother (Sally Field), he leads anything but a restricted life."
     }
 ]
--- src/pages/romcompage.js
+++ src/pages/romcompage.js
@@ -1,22 +1,83 @@
-import React from 'react'
-import { romcomlst } from '@/data/romcommvs'
+import React, { useState } from "react";
+import { romcomlst } from "@/data/romcommvs";
+import { SiImdb } from "react-icons/si";
 
 function romcompage() {
+  const [hover, setHover] = useState(false);
+  const [bgImage, setBgImage] = useState(
+    "https://png.pngtree.com/thumb_back/fw800/background/20220215/pngtree-retro-video-camera-poster-of-film-and-television-festival-image_926071.jpg"
+  );
+  const [titleDet, settitleDet] = useState("");
+  const [descDet, setdescDet] = useState("");
+  const [dirctr, setDirctr] = useState("");
+  const [yearDet, setyearDet] = useState("");
   return (
-    <div>
-    <div>romcompage</div>
-    {
-        romcomlst.map((mvlst) =>(
-          <div className='pl-3 pb-5'>
-            <a href="/testpg" target="_blank"><p className=' pt-5 pb-5'>{mvlst.title}</p>
-                <img width="250px" height="250px" className="card-img-top pb-5 " src={mvlst.image} alt={mvlst.alt} />
-            </a>
-            <p>{mvlst.desc}</p>
+    /*<div className="h-[100vh] bg-sky-500 ">*/
+    <div
+      className=" min-h-screen bg-cover duration-500 relative"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="genredes bg-gradient-to-t from-black     ">
+        <div className="genredes bg-gradient-to-t from-black pl-5">
+          <div className="grid grid-rows-2 min-h-screen grid-flow-col">
+            <div className=" pl-5 p-15 absolute text-left duration-500 w-2/4">
+              <div className="text-9xl text-white font-bold font-carterfont">
+                {titleDet}
+              </div>
+              <div className=" text-3xl text-white font-normal  pt-3">
+                {yearDet}
+              </div>
+              {hover && (
+                <div className=" text-3xl text-white font-normal  pt-3 ">
+                  <SiImdb /> {descDet}
+                </div>
+              )}
+              {hover && (
+                <div className=" text-3xl text-white font-normal pb-3 ">
+                  Director: {dirctr}
+                </div>
+              )}
+              {hover && (
+                <button class=" bg-black hover:scale-105 text-white font-semibold hover:text-white py-2 px-4 border border-black hover:border-transparent rounded">
+                  Learn More
+                </button>
+              )}
+            </div>
+
+            <div>
+            <div className="flex flex-row overflow-scroll flex-shrink-0">
+              {romcomlst.map((mvlst) => (
+                <div className=" p-2 ">
+                  <div
+                    className={`relative hover:scale-110 border-solid border-black `}
+                    onMouseEnter={() => {
+                      setBgImage(mvlst.hovimg);
+                      settitleDet(mvlst.title);
+                      setDirctr(mvlst.dirctr);
+                      setdescDet(mvlst.imdb);
+                      setyearDet(mvlst.year);
+                      setHover(true);
+                    }}
+                    onMouseLeave={() => setHover(true)}
+                  >
+                    <div className=" overflow-hidden shadow-xl min-h-[200px] min-w-[200px] ">
+                      <img
+                        
+                        className="rounded-2xl object-cover "
+                        src={mvlst.image}
+                        alt="Card Image"
+                      />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            </div>
           </div>
-        ))
-      }
+        </div>
       </div>
-  )
+    </div>
+  );
 }
 
-export default romcompage
+export default romcompage;
