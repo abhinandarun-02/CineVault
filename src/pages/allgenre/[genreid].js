dd5a48dc8e7efbaa5f1986bcacc660675b06a662
Uday Krishnan <udaykrishnanvg@gmail.com>
2023-01-22 19:57:52 +0530

    Zoom issue final fix

--- src/pages/allgenre/[genreid].js
+++ src/pages/allgenre/[genreid].js
@@ -99,13 +99,13 @@ const Genrefunc=()=> {
     
     
   return (
-    <div>
+    <div className="bg-black">
     <div
       className=" min-h-screen bg-cover duration-500 relative "
       style={{ backgroundImage: `url(${bgImage})` }}
     >
-      <div className="bg-gradient-to-t from-black     ">
-        <div className="bg-gradient-to-r from-black ">
+      <div className="min-h-screen bg-gradient-to-t from-black     ">
+        <div className="min-h-screen bg-gradient-to-r from-black ">
         {/* Menu-Bar */}
       <AnimatePresence>
         {menu && (
@@ -252,10 +252,10 @@ const Genrefunc=()=> {
           }}
         />
       </div>
-          <div className="grid grid-rows-2 h-screen grid-flow-col gap-6 ">
-            <div className=" pl-5 p-15 text-left duration-500 w-full sm:w-2/4  self-center">
+          <div className="genregrid  min-h-screen ">
+            <div className=" pl-5 p-15 text-left duration-500 w-full sm:w-2/4  self-center ">
             <div className="flex flex-col mt-10 sm:mt-8 sm:ml-12 sm:gap-3 gap-1">
-              <div className="text-3xl 2xl:text-6xl md:text-3xl lg:text-5xl xl:text-6xl   text-white font-bold font-carterfont ">
+              <div className="text-3xl 2xl:text-5xl md:text-3xl lg:text-5xl xl:text-6xl   text-white font-bold font-carterfont ">
                 {titleDet}
               </div>
               <div className=" sm:text-3xl text-lg 2xl:text-2xl lg:text-xl md:text-xl text-white font-normal  pt-3">
@@ -280,14 +280,14 @@ const Genrefunc=()=> {
               </div>
               )}
               {hover && (
-                <button style={{backgroundColor: "#00C896"}} className=" bg-black hover:scale-105 text-white font-bold hover:text-white p-2 border border-black hover:border-transparent rounded-2xl sm:w-[20%] w-[50%]">
+                <button style={{backgroundColor: "#00C896"}} className=" bg-black hover:scale-105 text-white font-bold hover:text-white p-2 border border-black hover:border-transparent rounded-2xl lg:w-[20%] sm:w-[50%] w-[150px]">
                   Learn More
                 </button>
               )}
             </div>
             </div>
 
-            <div className="flex flex-row overflow-x-scroll scrollbar max-w-[100%] absolute bottom-0">
+            <div className="flex flex-row overflow-x-scroll scrollbar max-w-[100%] justify-center items-start min-h-[250px] md:min-h-[350px]">
               {dataset.map((mvlst) => (
                 <div className=" p-3 h-[50%]" key={mvlst.id}>
                   <div
